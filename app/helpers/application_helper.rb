eb40dddb2a9fb4e22d4f6a50c8fd4f567270d1a7
Alexander <alexander.zaycev@mail.ru>
2018-12-04 23:19:42 +0300

    Display the user name on the account page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def login_name(user)
     user.name || user.email
   end
+
+  def user_name(user)
+    if user.name.present?
+      user.name
+    else
+      'Имя ещё не заполнено.'
+    end
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resource :home, only: [:index]
+  #resources :users, only: [:show]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
